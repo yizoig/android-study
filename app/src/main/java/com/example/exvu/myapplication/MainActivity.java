f3f45b7ae7404eaeebe5166a203b94a114907706
exvu <923100406@qq.com>
2018-05-09 23:55:27 +0800

    加入代码规范文件

--- app/src/main/java/com/example/exvu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/exvu/myapplication/MainActivity.java
@@ -3,7 +3,15 @@ package com.example.exvu.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     *
+     */
+    private final int a = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
