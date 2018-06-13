ad3e88942a8d0eaad708e43723cd3abad1e1e4e3
exvu <923100406@qq.com>
2018-06-13 15:36:00 +0800

    聊天记录

--- app/src/main/java/com/example/exvu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/exvu/myapplication/MainActivity.java
@@ -1,75 +1,133 @@
 package com.example.exvu.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
 /**
  *
  */
-public class MainActivity extends BaseActivity implements View.OnClickListener {
+public class MainActivity extends BaseActivity {
+
+    private Socket socket;
+    private BufferedReader bufferedReader = null;
+    private PrintWriter printWriter;
+    private String content = "";
+    private StringBuilder stringBuilder;
+    private android.widget.TextView title;
+    private android.widget.LinearLayout footer;
+    private TextView txtshow;
+    private android.widget.EditText editor;
+    private android.widget.Button send;
+    private static final String HOST = "192.168.0.106";
+    private static final int PORT = 12345;
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    if (socket != null && socket.isConnected()) {
+                        if (!socket.isInputShutdown() && bufferedReader != null) {
+                            if ((content = bufferedReader.readLine()) != null) {
+                                content += '\n';
+                                handler.sendEmptyMessage(0x002);
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+    public Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0x001:
+                    if (socket != null && socket.isConnected()) {
+                        new Thread() {
+                            @Override
+                            public void run() {
+                                String str = editor.getText().toString();
+                                try {
+
+                                    printWriter.println(str);
 
 
-    private long exitTime = 0;
-    private WebView webview;
-    private android.widget.Button back;
-    private android.widget.Button refresh;
-    private android.widget.Button top;
-    private TextView web_title;
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }.start();
+                        editor.setText("");
+                    }
 
+                    break;
+                case 0x002:
+                    stringBuilder.append(content);
+                    txtshow.setText(stringBuilder.toString());
+                    break;
+            }
+        }
+    };
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.webview = (WebView) findViewById(R.id.webview);
-        this.web_title = (TextView) findViewById(R.id.web_title);
-        this.top = (Button) findViewById(R.id.top);
-        this.refresh = (Button) findViewById(R.id.refresh);
-        this.back = (Button) findViewById(R.id.back);
+        this.send = (Button) findViewById(R.id.send);
+        this.editor = (EditText) findViewById(R.id.editor);
+        this.txtshow = (TextView) findViewById(R.id.txtshow);
+        this.footer = (LinearLayout) findViewById(R.id.footer);
+        this.title = (TextView) findViewById(R.id.title);
 
-        this.top.setOnClickListener(this);
-        this.refresh.setOnClickListener(this);
-        this.back.setOnClickListener(this);
 
-        webview.loadUrl("http://www.baidu.com");
-        webview.setWebChromeClient(new WebChromeClient() {
+        stringBuilder = new StringBuilder();
+
+
+        new Thread() {
             @Override
-            public void onReceivedTitle(WebView view, String title) {
-                super.onReceivedTitle(view, title);
-                web_title.setText(title);
+            public void run() {
+
+                try {
+                    socket = new Socket(HOST, PORT);
+                    bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+                    printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.sendEmptyMessage(0x001);
+
             }
         });
-
-        webview.setWebViewClient(new WebViewClient(){
-            //在webview里打开新链接
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        });
-
+        new Thread(runnable).start();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.back:
-                finish();
-                break;
-            case R.id.refresh:
-                webview.reload();
-                break;
-            case R.id.top:
-                webview.setScrollY(0);
-                break;
-        }
-    }
 }
 
